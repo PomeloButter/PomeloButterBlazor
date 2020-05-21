044fa759fdf1d6c760efe729d18b817318414491
PomeloButter <34675102+PomeloButter@users.noreply.github.com>
2020-05-21 22:03:46 +0800

    init project files

---
+++ src/PomeloButterBlog.App/wwwroot/js/common.js
@@ -0,0 +1,13 @@
+﻿
+function mobileBtn() {
+    var toggleMenu = document.getElementsByClassName("menu-toggle")[0];
+    var mobileMenu = document.getElementById("mobile-menu");
+    if (toggleMenu.classList.contains("active")) {
+        toggleMenu.classList.remove("active");
+        mobileMenu.classList.remove("active");
+    } else {;
+        toggleMenu.classList.add("active");
+        mobileMenu.classList.add("active");
+    }
+}
+
